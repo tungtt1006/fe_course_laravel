738b24604403ed2a1fc1b1a2421e56836c89301e
tungtt <toilatung52@gmail.com>
2021-11-18 09:33:31 +0700

    Làm thêm nav bên trên

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,8 +1,21 @@
 <template>
 <div>
+    <nav class="navbar navbar-expand-sm" style="background-color: white;">
+        <div class="col-md-8">
+            <h1 class="ps-3 m-0 fw-normal" style="color:#00c292;cursor: default;">KOURSES</h1>
+        </div>
+        <div class="col-md-2 ps-3">
+            <p class="m-0 fw-thin">Email</p>
+            <p class="m-0 fs-5 fw-normal">team13@gmail.com</p>
+        </div>
+        <div class="col-md-2 ps-5">
+            <p class="m-0 fw-thin">Hotline</p>
+            <p class="m-0 fs-5 fw-normal">0968.68.68.68</p>
+        </div>
+    </nav>
     <nav class="navbar navbar-expand-lg header">
         <div class="container-fluid">
-            <router-link class="navbar-brand nav_brand" to="/">KOURSES</router-link>
+            <router-link class="navbar-brand nav_brand" to="/">Home</router-link>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item dropdown">
