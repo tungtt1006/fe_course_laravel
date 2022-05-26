8e8d6ac9a26a93996c467851d7dffd287c2a0488
zinza <toilatung52@gmail.com>
2022-05-26 18:01:23 +0700

    Create

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -12,7 +12,7 @@
                     Register Successfully - Check your mail now!!!!
                 </div>
 
-                <h1 class="mt-4 fw-bolder text-success">{{ product.name }}</h1>
+                <h1 class="mt-4 display-5 fw-normal text-success">{{ product.name }}</h1>
                 <div class="w-100 text-break">{{ product.description }}</div>
 
                 <h2 class="mt-5 fw-normal">Nội dung khóa học</h2>
@@ -69,7 +69,7 @@
 
             <div class="col-4 p-4">
                 <div class="card border-0" v-if="theClass">
-                    <img :src="product.photo_url" class="card-img-top" alt="...">
+                    <img :src="product.photo_url" class="card-img-top border shadow-sm" alt="...">
                     <div class="card-body text-center">
                         <p class="card-text fs-4 text-muted text-decoration-line-through m-0">{{ formatNumber(product.price) }} VND</p>
                         <h3 class="card-title m-0">{{ discountPrice }} VND</h3>
---
+++ src/views/TeacherList.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="py-3">
+    <div class="container pb-5">
+        <div class="row text-center">
+            <div class="col-lg-8 mx-auto">
+                <h1 class="display-4 text-success">Giáo viên</h1>
+                <p class="lead mb-0">Đội ngũ giáo viên chất lượng cao, trình độ đỉnh của chóp, có nhiều năm kinh nghiệm.</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-4">
+                <div class="card mb-3 shadow-sm">
+                    <div class="row g-0">
+                        <div class="col-md-4 d-flex align-items-center">
+                            <img
+                                src="http://localhost:8000/storage/img/avatar/avatar.jpg"
+                                class="img-fluid rounded-circle img-thumbnail w-75"
+                                alt="Fail avatar"
+                            >
+                        </div>
+                        <div class="col-md-8 border">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
